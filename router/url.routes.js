6c784c847af380b511094f9780111d0875a3b88c
shreyashbijlwanOP <shreyash.bijlwanmail@gmail.com>
2024-02-23 06:51:20 +0530

    added analytic api

--- router/url.routes.js
+++ router/url.routes.js
@@ -3,12 +3,15 @@ const {
   createUrlShortId,
   redirectToUrl,
   getAllUrls,
+  getAnalytic,
 } = require("../controller/url.controller");
 
 const urlRouter = Router();
 
 urlRouter.route("/").get(getAllUrls).post(createUrlShortId);
 
-urlRouter.get("/:id", redirectToUrl);
+urlRouter.route("/:id").get(redirectToUrl);
+
+urlRouter.route("/analytic/:id").get(getAnalytic);
 
 module.exports = { urlRouter };
